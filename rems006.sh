7e161d82c1e80e955cd3cccc2b15bfe155abfa55
rems006 <Zach.Schultz.2.0.1@gmail.com>
2024-09-25 21:00:29 -0400

    adding rems006/7.sh

---
+++ rems006.sh
@@ -0,0 +1,4 @@
+cd ~/Github/rems006-Arduino/
+git add .
+git commit -m ".sh upload"
+git push -u origin main
---
+++ rems007.sh
@@ -0,0 +1,4 @@
+cd ~/Github/rems07/
+git add .
+git commit -m "sh upload"
+git push -u origin main
