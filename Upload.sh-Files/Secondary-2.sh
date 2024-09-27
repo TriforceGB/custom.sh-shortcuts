ced079a9bd9043efaffc51c2afc93ef639376e9c
Zach Schultz <Zach.Schultz.2.0.1@gmail.com>
2024-09-27 12:10:10 -0400

    Update Secondary-2.sh

--- Upload.sh-Files/Secondary-2.sh
+++ Upload.sh-Files/Secondary-2.sh
@@ -1,6 +1,9 @@
 echo doing git pull
 cd /home/rems006/Github/rems006-Arduino
-git pull
+git reset --hard HEAD
+git fetch --all
+git reset --hard origin/main
+
 echo running .ino
 cd Secondary-2
 arduino-cli compile --fqbn arduino:avr:nano Secondary-2.ino
