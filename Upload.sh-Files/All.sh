6e888849a934f0cbbe3c4f3f4e3546370def0af8
Zach Schultz <Zach.Schultz.2.0.1@gmail.com>
2024-10-01 11:49:21 -0400

    Chance to match phyical Boards

--- Upload.sh-Files/All.sh
+++ Upload.sh-Files/All.sh
@@ -11,9 +11,9 @@ arduino-cli upload -p/dev/ttyUSB1 --fqbn arduino:avr:nano Primary.ino
 cd ..
 cd Secondary-1
 arduino-cli compile --fqbn arduino:avr:nano Secondary-1.ino
-arduino-cli upload -p/dev/ttyUSB2 --fqbn arduino:avr:nano Secondary-1.ino
+arduino-cli upload -p/dev/ttyUSB0 --fqbn arduino:avr:nano Secondary-1.ino
 cd ..
 cd Secondary-2
 arduino-cli compile --fqbn arduino:avr:nano Secondary-2.ino
-arduino-cli upload -p/dev/ttyUSB0 --fqbn arduino:avr:nano Secondary-2.ino
+arduino-cli upload -p/dev/ttyUSB2 --fqbn arduino:avr:nano Secondary-2.ino
 
--- Upload.sh-Files/Secondary-2.sh
+++ Upload.sh-Files/Secondary-2.sh
@@ -7,5 +7,5 @@ git reset --hard origin/main
 echo running .ino
 cd Secondary-2
 arduino-cli compile --fqbn arduino:avr:nano Secondary-2.ino
-arduino-cli upload -p/dev/ttyUSB0 --fqbn arduino:avr:nano Secondary-2.ino
+arduino-cli upload -p/dev/ttyUSB2 --fqbn arduino:avr:nano Secondary-2.ino
 
