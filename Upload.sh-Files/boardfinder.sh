ce818fe19a873c105db4fae5309f3ad1ef1bea59
Zach Schultz <Zach.Schultz.2.0.1@gmail.com>
2024-10-03 13:36:20 -0400

    made boardfinder use current finder

--- Upload.sh-Files/boardfinder.sh
+++ Upload.sh-Files/boardfinder.sh
@@ -5,17 +5,15 @@ git reset --hard HEAD
 git fetch --all
 git reset --hard origin/main
 
-
 echo running .ino
 cd Primary
 arduino-cli compile --fqbn arduino:avr:nano Primary.ino
-arduino-cli upload -p/dev/ttyUSB0 --fqbn arduino:avr:nano Primary.ino
+arduino-cli upload -p/dev/${Primary} --fqbn arduino:avr:nano Primary.ino
 cd ..
 cd Secondary-1
 arduino-cli compile --fqbn arduino:avr:nano Secondary-1.ino
-arduino-cli upload -p/dev/ttyUSB1 --fqbn arduino:avr:nano Secondary-1.ino
+arduino-cli upload -p/dev/${Secondary1} --fqbn arduino:avr:nano Secondary-1.ino
 cd ..
 cd Secondary-2
 arduino-cli compile --fqbn arduino:avr:nano Secondary-2.ino
-arduino-cli upload -p/dev/ttyUSB2 --fqbn arduino:avr:nano Secondary-2.ino
-
+arduino-cli upload -p/dev/${Secondary2} --fqbn arduino:avr:nano Secondary-2.ino
