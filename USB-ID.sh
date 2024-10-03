bc4438b2e382e47d33ce0db975a1e6ebd84ef537
Zach Schultz <Zach.Schultz.2.0.1@gmail.com>
2024-10-03 13:07:54 -0400

    messing up usb id for testing

--- USB-ID.sh
+++ USB-ID.sh
@@ -1,4 +1,9 @@
 # USB-ID.sh
-Primary="ttyUSB1"
-Secondary1="ttyUSB0"
+Primary="ttyUSB0"
+Secondary1="ttyUSB1"
 Secondary2="ttyUSB2"
+
+
+# Primary="ttyUSB1"
+# Secondary1="ttyUSB0"
+# Secondary2="ttyUSB2"
