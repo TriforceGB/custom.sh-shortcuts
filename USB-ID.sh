525a43edc939069468b4311ff932ff8140890c7b
Zach Schultz <Zach.Schultz.2.0.1@gmail.com>
2024-10-03 12:53:25 -0400

    changed bash-alias

--- USB-ID.sh
+++ USB-ID.sh
@@ -1,4 +1,4 @@
 # USB-ID.sh
 Primary="ttyUSB1"
-Secondary-1="ttyUSB0"
-Secondary-2="ttyUSB2"
+Secondary1="ttyUSB0"
+Secondary2="ttyUSB2"
--- bash-alias.sh
+++ bash-alias.sh
@@ -1,10 +1,10 @@
 #bash-alias.sh
 #these are alias I add to my .bashrc file
-source /home/rems006/Github/custom.sh-shortcuts
+source /home/rems006/Github/custom.sh-shortcuts/USB-ID.sh
 
-alias stp="arduino-cli monitor -p /dev/${Secondary}-1 -b arduino:avr:nano"
-alias st1="arduino-cli monitor -p /dev/${Secondary-1} -b arduino:avr:nano"
-alias st2="arduino-cli monitor -p /dev/${Secondary-2} -b arduino:avr:nano"
+alias stp="arduino-cli monitor -p /dev/${Primary} -b arduino:avr:nano"
+alias st1="arduino-cli monitor -p /dev/${Secondary1} -b arduino:avr:nano"
+alias st2="arduino-cli monitor -p /dev/${Secondary2} -b arduino:avr:nano"
 
 alias up="/home/rems006/Github/custom.sh-shortcuts/Upload.sh-Files/Primary.sh"
 alias u1="/home/rems006/Github/custom.sh-shortcuts/Upload.sh-Files/Secondary-1.sh"
@@ -13,3 +13,7 @@ alias ua="/home/rems006/Github/custom.sh-shortcuts/Upload.sh-Files/All.sh"
 
 
 alias boardfinder="/home/rems006/Github/custom.sh-shortcuts/Upload.sh-Files/boardfinder.sh"
+
+echo ${Primary}
+echo ${Secondary1}
+echo ${Secondary2}
