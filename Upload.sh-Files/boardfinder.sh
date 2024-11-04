d3b25b6e4747ba5b11eea092158277e3e5f50f41
Zach Schultz <Zach.Schultz.2.0.1@gmail.com>
2024-11-04 12:46:20 -0500

    Testing New Boardfinder

--- Upload.sh-Files/boardfinder.sh
+++ Upload.sh-Files/boardfinder.sh
@@ -4,18 +4,9 @@ cd /home/rems006/Github/rems006-Arduino/Code-Collection/Board-Finder
 
 echo running .ino
 echo --------------
-echo Primary Blinking
-echo Secondary-1 OFF
-echo Secondary-2 ON
+echo Type Something into Terminal to Get Board to Changed LED States
 echo --------------
-cd Primary
-arduino-cli compile --fqbn arduino:avr:nano Primary.ino
-arduino-cli upload -p/dev/${Primary} --fqbn arduino:avr:nano Primary.ino
-cd ..
-cd Secondary-1
-arduino-cli compile --fqbn arduino:avr:nano Secondary-1.ino
-arduino-cli upload -p/dev/${Secondary1} --fqbn arduino:avr:nano Secondary-1.ino
-cd ..
-cd Secondary-2
-arduino-cli compile --fqbn arduino:avr:nano Secondary-2.ino
-arduino-cli upload -p/dev/${Secondary2} --fqbn arduino:avr:nano Secondary-2.ino
+arduino-cli compile --fqbn arduino:avr:nano Board-Finder.ino
+arduino-cli upload -p/dev/${Primary} --fqbn arduino:avr:nano Board-Finder.ino
+arduino-cli upload -p/dev/${Secondary1} --fqbn arduino:avr:nano Board-Finder.ino
+arduino-cli upload -p/dev/${Secondary2} --fqbn arduino:avr:nano Board-Finder.ino
